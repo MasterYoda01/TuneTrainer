1bc08921314a43639dbc09bcab857ae6031c0bb4
Promia Chowdhury <promia@mit.edu>
2023-11-28 21:31:33 -0500

    login made nicer hehe

--- client/components/Login/RegisterForm.vue
+++ client/components/Login/RegisterForm.vue
@@ -30,41 +30,20 @@ async function register() {
           <input v-model.trim="username" type="text" id="aligned-name" placeholder="Username" required />
         </div>
         <div class="pure-control-group">
-          <input type="password" v-model.trim="password" id="aligned-password" placeholder="Password" required />
+          <input type="text" v-model.trim="password" id="aligned-password" placeholder="Password" required />
         </div>
-        <br />
         <div class="button-container">
           <button type="submit" class="login-button">Register</button>
         </div>
+        <p />
       </div>
     </fieldset>
   </form>
 </template>
 
 <style scoped>
-input[type="text"],
-input[type="password"] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+.pure-control-group{
+  margin-bottom: -2%;
 }
 
-button[type="submit"] {
-  width: 100%;
-  background-color: #d9cafa;
-  color: black;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: 1px solid black;
-  border-color: black;
-  font-weight: 550;
-  border-radius: 5px;
-}
-
-button[type="submit"]:hover {
-  background-color: #b69bee;
-}
 </style>
