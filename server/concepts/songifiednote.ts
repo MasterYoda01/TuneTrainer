a5354aa2f35dcf2d8a0ae5faa284da955c5b7826
Dima Yanovsky <yanovsky00@gmail.com>
2023-11-28 20:59:37 -0500

    new generate song features

--- server/concepts/songifiednote.ts
+++ server/concepts/songifiednote.ts
@@ -3,19 +3,24 @@ import DocCollection, { BaseDoc } from "../framework/doc";
 
 export interface SongifiedNoteDoc extends BaseDoc {
   author: ObjectId;
-  rawnote: string;
+  rawNote: string;
   generatedLyrics: string;
   lyricsTemplate: string;
   backgroundMusic: string; //link to the songs on the server
-  quizCard: string;
+  // quizCard: string; //actually let's omit this
 }
 
 export default class SongifiedNoteConcept {
-  public readonly users = new DocCollection<SongifiedNoteDoc>("songifiednote");
+  public readonly songifiednotes = new DocCollection<SongifiedNoteDoc>("songifiednote");
 
-  // async generateSongifiedNote(rawNote: string, songTemplate: string, author: ObjectId) {
-  //   const generatedLyrics = await
-  // }
+  async createSongifiedNote(author: ObjectId, rawNote: string, generatedLyrics: string, lyricsTemplate: string) {
+    const _id = await this.songifiednotes.createOne({ author: author, rawNote: rawNote, generatedLyrics: generatedLyrics, lyricsTemplate: lyricsTemplate });
+    return await this.songifiednotes.readOne({ _id });
+  }
+
+  async deleteSongifiedNote(_id: string) {
+    return this.songifiednotes.deleteOne({ _id: new ObjectId(_id) });
+  }
 
   // async create(username: string, password: string) {
   //   await this.canCreate(username, password);
