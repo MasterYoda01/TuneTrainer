2afec68c44cbe9d77b1aac0f58f33c7340378f55
Promia Chowdhury <promia@mit.edu>
2023-11-30 13:51:22 -0500

    reformatted smartcollections to accept generic username

--- client/components/SmartCollection/MultiCollectionsView.vue
+++ client/components/SmartCollection/MultiCollectionsView.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
-import { useUserStore } from "@/stores/user";
-import { storeToRefs } from "pinia";
-import { onMounted, ref } from "vue";
+import { defineProps, onMounted, ref } from "vue";
 
 import { fetchy } from "../../utils/fetchy";
 
-const userStore = useUserStore();
+const props = defineProps(["collections"]);
+const collections = props.collections;
 
-const { currentUsername } = storeToRefs(userStore);
-
-const titleParam = ref("");
-const descriptionParam = ref("");
 const collectionIdParam = ref("");
 const songIdParam = ref("");
 
@@ -25,21 +20,7 @@ interface SongCollectionDoc {
 
 const userCollections = ref<SongCollectionDoc[]>([]);
 //PROMI -- use this function to make add new collection feature
-async function createCollection() {
-  try {
-    let title = titleParam.value;
-    let description = descriptionParam.value;
 
-    let query = { title, description };
-
-    const response = await fetchy(`/api/create/collection`, "POST", { query });
-
-    alert(`Collection created: ${response.msg}`);
-    // Refresh collections list or handle response
-  } catch (error) {
-    alert("Error creating collection");
-  }
-}
 
 // we will need this later in the code where a single collection is rendered
 async function addSongToCollection() {
@@ -63,13 +44,7 @@ async function addSongToCollection() {
 
 //PROMI -- this function gets all the user's collections
 const getUsersCollections = async () => {
-  try {
-    const response = await fetchy(`/api/collections/${currentUsername.value}`, "GET", {});
-    console.log(response);
-    userCollections.value = response;
-  } catch (error) {
-    console.error("Error getting collection notes:", error);
-  }
+  
 };
 
 onMounted(async () => {
@@ -79,20 +54,9 @@ onMounted(async () => {
 
 <template>
   <div>
-    <div>
-      <h4>Create New Collection</h4>
-      <form @submit.prevent="createCollection">
-        <input v-model="titleParam" type="text" placeholder="Title" required />
-        <input v-model="descriptionParam" type="text" placeholder="Description" required />
-        <button type="submit">Create</button>
-      </form>
-    </div>
-  </div>
-
-  <div>
-    <h4>My Collections</h4>
+    <h3>My Collections</h3>
     <div class="collections-container">
-      <div v-for="collection in userCollections" :key="collection._id" class="collection-block">
+      <div v-for="collection in collections" :key="collection._id" class="collection-block">
         <h5>{{ collection.title }}</h5>
         <p>{{ collection.description }}</p>
         <p>Number of Songs: {{ collection.songifiedNotes.length }}</p>
--- client/views/SmartCollectionsView.vue
+++ client/views/SmartCollectionsView.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
 import AllSmartCollectionsComponent from "@/components/SmartCollection/AllSmartCollectionsComponent.vue";
-import UserSmartCollectionsComponent from "@/components/SmartCollection/UserSmartCollectionsComponent.vue";
+import CreateCollectionComponent from "../components/SmartCollection/CreateCollectionComponent.vue";
+// import UserSmartCollectionsComponent from "@/components/SmartCollection/UserSmartCollectionsComponent.vue";
+import MultiCollectionsView from "../components/SmartCollection/MultiCollectionsView.vue";
+
+import router from "@/router";
+import { useUserStore } from "@/stores/user";
+import { storeToRefs } from "pinia";
+import { onBeforeMount, ref } from "vue";
+import { fetchy } from "../utils/fetchy";
+
+const userStore = useUserStore();
+const { currentUsername } = storeToRefs(userStore);
+
+const user = ref(router.currentRoute.value.params.user);
+const loaded = ref(false); 
+const collections = ref<Array<Record<string, string>>>([]); 
+
+//finds all collections owned/shared by user 
+onBeforeMount(async () => {
+  try {
+    const response = await fetchy(`/api/collections/${user.value}`, "GET", {});
+    console.log("smart collections view", response);
+    collections.value = response;
+  } catch (error) {
+    console.error("Error getting collection notes:", error);
+  } finally{
+    loaded.value = true; 
+  }
+}); 
+
 </script>
 
 <template>
-  <h3 class="global-title">SmartCollections</h3>
-  <section class="global-page-layout">
-    <UserSmartCollectionsComponent />
+  <main class="container">
+    <h3 class="major-labels">Smart <span style="color:#000">Collections</span></h3>
+    <CreateCollectionComponent v-if="user == currentUsername"/>
+    <MultiCollectionsView v-if="loaded" :collections="collections"/>
     <AllSmartCollectionsComponent />
-  </section>
+  </main>
 </template>
 
 <style scoped>
