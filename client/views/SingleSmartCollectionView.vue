d99c845045f63e8325cb43aa23ecf56a1c2b92d7
Promia Chowdhury <promia@mit.edu>
2023-11-30 17:23:59 -0500

    before pull

--- client/views/SingleSmartCollectionView.vue
+++ client/views/SingleSmartCollectionView.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import SingleSmartCollectionComponent from "@/components/SmartCollection/SingleSmartCollectionComponent.vue";
+import router from "@/router";
+import { onBeforeMount, ref } from "vue";
+
+const collection_id = ref(router.currentRoute.value.params.id);
+const loaded = ref(false); 
+
+onBeforeMount(async () => {
+  // get collection information by collection_id 
+  loaded.value = true; 
+});
+
 </script>
 
 <template>
-  <main>
-    <h3 class="global-title">SmartCollection</h3>
+  <main class="container">
+    <h2 class="global-title">Smart Collection</h2>
     <section class="global-page-layout">
-      <SingleSmartCollectionComponent />
+      <SingleSmartCollectionComponent v-if="loaded"/>
     </section>
   </main>
 </template>
